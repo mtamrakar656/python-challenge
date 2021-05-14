5980a4e3edf4ad2a38b47bd6baf20619c592e135
mtamrakar656 <madhusudan.mtamrakar@gmail.com>
2021-05-14 17:00:26 -0400

    changes to PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,25 +3,18 @@ import os
 import csv
 
 #Declare variables:
-total_months = 0
-net_amount = 0
-month_of_change = []
-revenue_change_list = []
-greatest_increase = ["", 0]
-greatest_decrease = ["", 9999999999999999999]
+total_votes = 0
 
 #Assign a variable for filepath
-file_path = os.path.join(".", "Resources/budget_data.csv")
+file_path = os.path.join(".", "Resources/election_data.csv")
 
 #Read the contents of the file
-with open(file_path, newline = '') as csvfile:
-	csvreader = csv.reader(csvfile, delimiter = ",")
+with open(file_path) as electionfile:
+	csvreader = csv.reader(electionfile)
 
 	for row in csvreader:
-		#Count the totals
-		total_months = total_months + 1
-		net_amount = net_amount + int(row[1])
+		total_votes = total_votes + 1
 
-print(f"Total Months:  {total_months}")
-print(f"Total: {net_amount}" )
+
+
 
